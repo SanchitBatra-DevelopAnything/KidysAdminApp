c90c6c893c5d18a112a75f0c155d6efde8e9debd
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2024-09-04 20:25:22 +0530

    Fixing the notification API

--- functions/index.js
+++ functions/index.js
@@ -5,24 +5,46 @@ const admin = require('firebase-admin');
 const corsModule = require('cors');
 admin.initializeApp();
 
-const { messaging } = admin;
+
+
 const cors = corsModule({ origin: true });
+
 
 // Cloud Function to send a push notification with an HTTP trigger
 exports.sendApprovalNotification = functions.https.onRequest((req, res) => {
   cors(req, res, async () => {
     try {
       const { deviceToken, title, matter } = req.body;
+      const messaging = admin.messaging();
+
+      if(!messaging)
+      {
+        console.error("Admin SDK Not set up correctly");
+        return;
+      }
 
       const payload = {
         notification: {
           title: title,
           body: matter,
-          sound: 'default',
         },
+        android: {
+          notification: {
+              sound: 'default',  // Plays the default notification sound on Android
+          },
+      },
+      apns: {
+          payload: {
+              aps: {
+                  sound: 'default',  // Plays the default notification sound on iOS
+              },
+          },
+      },
+        token : deviceToken
       };
 
-      const response = await messaging().sendToDevice(deviceToken, payload);
+      
+      const response = await messaging.send(payload);
 
       console.log('Notification sent successfully:', response);
 
@@ -30,7 +52,7 @@ exports.sendApprovalNotification = functions.https.onRequest((req, res) => {
     } catch (error) {
       console.error('Error sending notification:', error);
 
-      res.status(500).json({ error: 'Error sending notification' });
+      res.status(500).json({ error: 'Error sending notification'});
     }
   });
 });
